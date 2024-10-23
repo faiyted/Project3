431ff5cc1a5d26fb4baa051209aa8887fb0f561b
lsunley <lsunley@gmail.com>
2024-10-23 11:17:18 -0500

    commit

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 import pandas as pd
 import matplotlib.pyplot as plt
+
 # Running Flask
 # Step 1 pip install flask pymongo
 # Step 2  pip install flask pymongo pandas
+
 # Initialize Flask app
 app = Flask(__name__)
 
@@ -12,7 +14,6 @@ app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
 db = client["remote"]
 collection = db["collectionName"]
-
 
 @app.route('/')
 def home():
@@ -26,21 +27,6 @@ def mongo_data():
 
         # Convert MongoDB cursor to JSON
         return jsonify(data), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-@app.route('/csv_to_json', methods=['GET'])
-def csv_to_json():
-    try:
-        # Load the CSV file
-        df = pd.read_csv("Impact_of_Remote_Work_on_Mental_Health.csv")
-        df = df.where(pd.notnull(df), None)
-        
-        # Convert DataFrame to JSON
-        data_json = df.to_dict(orient='records')
-
-        return jsonify(data_json), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
